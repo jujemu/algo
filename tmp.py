2e61c9f6931e771deee3c64dbb296f05d30754a3
jujemu <jujemu30@gmail.com>
2023-02-28 20:03:01 +0900

    Practice for code test 2.28

--- tmp.py
+++ tmp.py
@@ -1,7 +1,16 @@
+d = {
+    0: (1, 0),
+    1: (0, 1)
+}
+for n in range(2, 41):
+    zero, one = d[n-1]
+    zero += d[n-2][0]
+    one += d[n-2][1]
+    d[n] = zero, one
+    
 T = int(input())
 for _ in range(T):
-    R, s = input().split()
-    tmp = set(s)
-    for ele in tmp:
-        s = s.replace(ele, ele*int(R))
-    print(s)
+    result = 0
+    N = int(input())
+    print(d[N][0], d[N][1])
+        
